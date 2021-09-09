dfa776d9909d2519af2b163505cdecd7e64ca30f
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-09-09 11:30:46 +0600

    start learning react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique nostrum quo vitae sapiente? Temporibus iure facere neque accusamus deserunt, rem esse nulla, ad impedit velit suscipit quis perferendis quo cupiditate.</p>
+      <h1>learn react</h1>
     </div>
   );
 }
